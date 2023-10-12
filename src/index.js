8ad2c16019bee764f0dd9405955ae6c6b650f807
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-12 10:53:13 -0400

    Added functionality to time conversion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('Hello world');
+const express = require('express');
+const app = express();
+
+app.get('/api/', (req, res) => {
+    let dateNow = new Date();
+    res.json({
+        'unix': Date.now(),
+        'utc': dateNow.toUTCString()
+    });
+})
+
+app.get('/api/:date?', (req, res) => {
+    let date = req.params.date;
+
+    if(date.length != 0 && isNaN(date)) 
+    {
+        res.json({
+            'error': 'Invalid Date'
+        });
+    }
+
+    if(!isNaN(date)) 
+    {
+        let dateConverted = new Date(parseInt(date));
+        res.json({
+            'unix': parseInt(date),
+            'utc': dateConverted.toUTCString()
+        });
+    }
+
+    let dateNow = new Date.now();
+    res.json({
+        'unix': parseInt(dateNow),
+        'utc': dateConverted.toUTCString()
+    });
+})
+
+app.listen(3000);
+console.log('Server is listening on port', 3000);
