5be62009a17557bda89e10e45850dd873ad45844
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-16 13:42:38 -0400

    Added new functionality to time conversion

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ app.get('/api/', (req, res) => {
     });
 })
 
-app.get('/api/:date?', (req, res) => {
-    let date = req.params.date;
+app.get('/api/:date_string?', (req, res) => {
+    let date = req.params.date_string;
     let simpleDateFormat = "";
 
     if(date.includes("-") == false) 
